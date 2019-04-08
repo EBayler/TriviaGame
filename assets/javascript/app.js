f515d48deb27bde4462744931ded6e33d1990a7f
EBayler <erika.bayler@gmail.com>
2019-04-07 23:50:29 -0600

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+var questionsAnswers = [{
+    question: "In what movie does Brad Pitt play Lieutenant Aldo Raine?",
+    ChoiceA: "Inglorius Basterds",
+    choiceB: "Fight Club",
+    choiceC: " Bastard Out Of Carolina",
+    choiceD: "The Departed",
+    correct: "A",
+}, {
+    question: "In what movie does Bill say, \"I'm a killer, a murdering bastard, and there are consequences to breaking the heart of a murdering bastard \"?",
+    choiceA: "Bill and Ted's Excellent Adventure",
+    choiceB: "The Bill Collector",
+    choiceC: "Kill Bill Vol. 2",
+    choiceD: "The Great Adventures of Wild Bill Hickock",
+    correct: "C",
+
+}, {
+    question: "In the HBO Television Series Game of Thrones who said, \"I have a tender spot in my heart for cripples, bastards and broken things\"?",
+    choiceA: "Sansa Stark",
+    choiceB: "Tyrion Lannister",
+    choiceC: "Cercei Lannister",
+    choiceD: "Jaime Lannister",
+    correct: "B",
+
+}, {
+    question: "Which of these is a Shakespearean quote?",
+    choiceA: "\"The bastard brains with these proper hands shall I dash out\".",
+    choiceB: "\"I should of been that I am, had the maidenliest star in the firmament twinkled on my bastardizing\".",
+    choiceC: "\"I am a bastard too, I love bastards.\".",
+    choiceD: "All of the above.",
+    correct: "D",
+
+}, {
+    question: "Name that movie: \"Little bastard shot me in the ass\".",
+    choiceA: "Blazing Saddles",
+    choiceB: "Willy Wonka and the Chocolate Factory",
+    choiceC: "Robin Hood: Men In Tights",
+    choiceD: "Young Frankenstein",
+    correct: "A",
+
+}, {
+    question: "What movie is this argument from: \"Albert: Don’t use that tone to me. - Armand: What tone? - Albert : That sarcastic contemptuous tone.That means you know everything because you’re a man, and I know nothing because I’m a woman. - Armand: You’re not a woman. - Albert: Oh, you bastard!\"?",
+    choiceA: "Hedwig and the Angry Itch",
+    choiceB: "Birdcage",
+    choiceC: "To Wong Foo, Thanks for Everything! Julie Newmar",
+    choiceD: "Die Hard",
+    correct: "B",
+
+}, {
+    question: "Complete this quote from the BBC television show The I.T. Crowd: \"People, what a bunch of _______\".",
+    choiceA: "fudge dragons!",
+    choiceB: "marshmallows!",
+    choiceC: "snowflakes!",
+    choiceD: "bastards!",
+    correct: "D",
+
+}, {
+    question: "Which amazing Monty Python movie was this famous quote from: \"You lucky, lucky bastard...Proper little jailer's pet, aren't we!\"",
+    choiceA: "Life of Brian",
+    choiceB: "Monty Python and the Holy Grail",
+    choiceC: "The Meaning of Life",
+    choiceD: "And Now For Something Completely Different",
+    correct: "A",
+
+}, {
+
+}];
