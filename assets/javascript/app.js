a6d7070423efc07f769daea0a7607a648bd3ccbb
EBayler <erika.bayler@gmail.com>
2019-04-15 23:02:59 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,56 +4,56 @@ window.onload = function () {
             question: "In what movie does Brad Pitt play Lieutenant Aldo Raine?",
             choices: ["Inglorious Basterds", "Fight Club", " Bastard Out Of Carolina", "The Departed"],
             answer: 0,
-            photo: "assets/images/IngloriousBasterds.gif"
+            gif: "assets/images/IngloriousBasterds.gif"
         },
         {
             question: "In what movie does Bill say, \"I'm a killer, a murdering bastard, and there are consequences to breaking the heart of a murdering bastard \"?",
             choices: ["Bill and Ted's Excellent Adventure", "The Bill Collector", "Kill Bill Vol. 2", "The Great Adventures of Wild Bill Hickock"],
             answer: 2,
-            photo: "assets/images/KillBill.gif"
+            gif: "assets/images/KillBill.gif"
         },
         {
             question: "In the HBO Television Series Game of Thrones who said, \"I have a tender spot in my heart for cripples, bastards and broken things\"?",
             choices: ["Sansa Stark", "Tyrion Lannister", "Cercei Lannister", "Jaime Lannister"],
             answer: 1,
-            photo: "assets/images/Tyrion.gif"
+            gif: "assets/images/Tyrion.gif"
         },
         {
             question: "Which of these is a Shakespearean quote?",
             choices: ["\"The bastard brains with these proper hands shall I dash out\".", "\"I should of been that I am, had the maidenliest star in the firmament twinkled on my bastardizing\".", "\"I am a bastard too, I love bastards.\".", "All of the above."],
             answer: 3,
-            photo: "assets/images/shakespeare.gif"
+            gif: "assets/images/shakespeare.gif"
         },
         {
             question: "Name that movie: \"Little bastard shot me in the ass\".",
             choices: ["Blazing Saddles", "Willy Wonka and the Chocolate Factory", "Robin Hood: Men In Tights", "Young Frankenstein"],
             answer: 0,
-            photo: "assets/images/BlazingSaddles.gif"
+            gif: "assets/images/BlazingSaddles.gif"
         },
         {
             question: "What movie is this argument from: \"A: Don’t use that tone to me. - Ar: What tone? - A: That sarcastic contemptuous tone.That means you know everything because you’re a man, and I know nothing because I’m a woman. - Ar: You’re not a woman. - A: Oh, you bastard!\"?",
             choices: ["Hedwig and the Angry Itch", "Birdcage", "To Wong Foo, Thanks for Everything! Julie Newmar", "Die Hard"],
             answer: 1,
-            photo: "assets/images/Birdcage.gif"
+            gif: "assets/images/Birdcage.gif"
         },
         {
             question: "Complete this quote from the BBC television show The I.T. Crowd: \"People, what a bunch of _______\".",
             choices: ["fudge dragons!", "marshmallows!", "snowflakes!", "bastards!"],
             answer: 3,
-            photo: "assets/images/I.T.Crowd.gif"
+            gif: "assets/images/I.T.Crowd.gif"
         },
         {
             question: "Which amazing Monty Python movie was this famous quote from: \"You lucky, lucky bastard...Proper little jailer's pet, aren't we!\"",
             choices: ["Life of Brian", "Monty Python and the Holy Grail", "The Meaning of Life", "And Now For Something Completely Different"],
             answer: 0,
-            photo: "assets/images/LifeOfBrian.gif"
+            gif: "assets/images/LifeOfBrian.gif"
         },
         {
             question: "What police film is this quote from: \"You don't mind a bit of manpower, do ya Doris?...[laughing] Oh, dirty bastard!\"",
             choices: ["Let's Be Cops", "Zootopia", "Super Troopers", "Hot Fuzz"],
             answer: 3,
-            photo: "assets/images/HotFuzz.gif"
-        },
+            gif: "assets/images/HotFuzz.gif"
+        }
     ];
 
     var correctCount = 0;
@@ -122,24 +122,31 @@ window.onload = function () {
 
     }
 
-    $(".answerChoice").on("click", function() {
+    $(".answerChoice").on("click", function () {
         userGuess = parseInt($(this).attr("data-guessValue"));
         if (userGuess === pick.answer) {
             stop();
             correctCount++;
             userGuess = "";
-            $("#answerBlock").html("<p>Not likely! The correct answer is: " + pick.choices[pick.answer] + "</p>");
+            $("#answerBlock").html("<p>Correct!</p>");
+            hideGif();
+        } else {
+            stop();
+            wrongCount++;
+            userGuess = "",
+                $("#answerBlock").html("<p>Not likely! The correct answer is: " + pick.choices[pick.answer] + "</p>");
             hideGif();
         }
 
     })
 
+
     function hideGif() {
-        $("#answerBlock").append("<img src=" + pick.photo + ">");
+        $("#answerBlock").append("<img src=" + pick.gif + ">");
         newArray.push(pick);
         options.splice(index, 1);
 
-        var hideGif = setTimeout(function () {
+        var hiddenGif = setTimeout(function () {
             $("#answerBlock").empty();
             timer = 30;
 
@@ -162,46 +169,17 @@ window.onload = function () {
 
     }
 
-    $("#reset").on("click", function () {
-            $("#reset").hide();
-            $("#answerBlock").empty();
-            $("#questionBlock").empty();
 
-            for (var i = 0; i < holder.length; i++);
-            options.push(holder[i]);
-        })
-        runTimer();
+    $("#reset").on("click", function () {
+        $("#reset").hide();
+        $("#answerBlock").empty();
+        $("#questionBlock").empty();
+
+        for (var i = 0; i < holder.length; i++);
+        options.push(holder[i]);
+    })
+    runTimer();
     displayQuestion();
 
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var container = document.getElementById("#container");
-
-// for (var i = 0; i < questionsAnswers.length; i++);
-// var questionContainer = document.createElement('DIV');
-
-// questionContainer.textContent = questionsAnswers[i].question;
-
-// var options = questionsAnswers[i].choices;
-// for (var opt in options) {
-//     // $('<input type="radio" name="radiobtn"> ')
-
-//     console.log(options)
-// }
-// container.appendChild(questionContainer);
